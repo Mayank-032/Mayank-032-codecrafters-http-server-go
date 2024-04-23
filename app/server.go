cc833edc8a9a15942f89c672ef6eb3e94efaf994
Mayank Mohan <mayankmohan@Mayanks-MacBook-Air.local>
2024-04-23 17:39:18 +0530

    process and extract path for the request body

--- app/server.go
+++ app/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -22,12 +23,36 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
+
+	var reqByte = make([]byte, 4096)
+	reqByteSize, err := conn.Read(reqByte)
+	if err != nil {
+		fmt.Println("Error reading connection: ", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("req_bytes_size: %v, req_bytes: %v\n", reqByteSize, string(reqByte))
+
+	path := extractPath(reqByte)
 
 	response := "HTTP/1.1 200 OK\r\n\r\n"
+	if path != "/" {
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
 	_, err = conn.Write([]byte(response))
 	if err != nil {
 		fmt.Println("Error writing on connection: ", err.Error())
-		os.Exit(1)
+		os.Exit(1)                             
 	}
 	fmt.Println("successfully written on connection: ", response)
+}
+
+func extractPath(reqByte []byte) string {
+	reqBodySplitArr := strings.Split(string(reqByte), " ")
+	var path string
+	if len(reqBodySplitArr) > 1 {
+		path = reqBodySplitArr[1]
+	}
+	return path
 }
